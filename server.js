9cb62c06dd666390ca12d51bceb961c828b8b63e
noo-dev <noo.dev23@gmail.com>
2023-12-09 09:28:17 +0500

    replaced files

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const fs = require('fs').promises;
+const exists = require('fs').exists;
+const path = require('path');
 
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let userGoal = "Learn Docker!";
+const app = express();
 
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-)
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static("public"))
+app.use(express.static('public'));
+app.use('/feedback', express.static('feedback'));
 
-app.get("/", (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <link rel="stylesheet" href="style.css">
-            </head>
-            <body>
-                <section>
-                    <h2>My course Goal!</h2>
-                    <h3>${userGoal}</h3>
-                </section>
-                <form action="/store-goal" method="POST">
-                    <div class="form-control">
-                        <label>Course Goal</label>
-                        <input type="text" name="goal">
-                    </div>
-                    <button>Set course goal</button>
-                </form>
-            </body>
-        </html>
-    `)
-}) 
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'feedback.html');
+  res.sendFile(filePath);
+});
 
-app.post('/store-goal', (req, res) => {
-    const enteredGoal = req.body.goal;
-    console.log(enteredGoal);
-    userGoal = enteredGoal;
-    res.redirect("/");
-})
+app.get('/exists', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'exists.html');
+  res.sendFile(filePath);
+});
 
-app.listen(3000); 
+app.post('/create', async (req, res) => {
+  const title = req.body.title;
+  const content = req.body.text;
+
+  const adjTitle = title.toLowerCase();
+
+  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
+  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+
+  await fs.writeFile(tempFilePath, content);
+  exists(finalFilePath, async (exists) => {
+    if (exists) {
+      res.redirect('/exists');
+    } else {
+      await fs.rename(tempFilePath, finalFilePath);
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(80);
